1081da06ea0cdaaccfb634520af05523d6a5c8b1
Gabriel Santos <gabrielr@take.net>
2019-03-13 23:05:33 +0000

    Fix title name. Add missing text in json. Add text with label

--- models/mediaLinkDocument.py
+++ models/mediaLinkDocument.py
@@ -7,14 +7,14 @@ class MediaLinkDocument(Document):
     MIME_TYPE = 'application/vnd.lime.media-link+json'
 
     def __init__(self, mimeType=None, size=None, aspectRatio=None, uri=None,
-                 tittle=None, text=None, previewType=None, previewUri=None):
+                 title=None, text=None, previewType=None, previewUri=None):
         super().__init__(MediaType.Parse(MediaLinkDocument.MIME_TYPE))
 
         self.MimeType = mimeType
         self.Size = size
         self.AspectRatio = aspectRatio
         self.Uri = uri
-        self.Tittle = tittle
+        self.Title = title
         self.Text = text
         self.PreviewType = previewType
         self.PreviewUri = previewUri
@@ -29,12 +29,14 @@ class MediaLinkDocument(Document):
         }
         if self.MimeType is not None:
             json.update({'type': self.MimeType})
+        if self.Text is not None:
+            json.update({'text': self.Text})
         if self.Size is not None:
             json.update({'size': self.Size})
         if self.AspectRatio is not None:
             json.update({'aspectRatio': self.AspectRatio})
-        if self.Tittle is not None:
-            json.update({'tittle': self.Tittle})
+        if self.Title is not None:
+            json.update({'title': self.Title})
         if self.PreviewType is not None:
             json.update({'previewType': self.PreviewType})
         if self.PreviewUri is not None:
